0b5b98d9e2d3db298086b6d494775782d29307d0
feng <fengzhen66@outlook.com>
2018-04-22 15:47:24 +0800

    jsonp跨域demo

--- jack.js
+++ jack.js
@@ -1,3 +1,5 @@
+
+
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
@@ -5,24 +7,23 @@ var port = 8002
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
-  var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  var path = parsedUrl.pathname
-  var query = parsedUrl.query
+  var path = request.url 
+  var query = ''
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  var pathNoQuery = parsedUrl.pathname
+  var queryObject = parsedUrl.query
   var method = request.method
-
-  if(path === '/'){
+console.log(pathNoQuery)
+  if(pathNoQuery==='/'&&method==='GET'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`
-        ${query.callback}('hahahaha')
+      ${queryObject.callback}('hahahahahah')
     `)
     response.end()
-  }else{
+  }
+  else {
     response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('呜呜呜')
     response.end()
   }
 })
